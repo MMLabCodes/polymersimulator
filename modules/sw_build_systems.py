355664d8b632bc22bf1bf3bc175a51de27163a68
DanielYyork <daniel.yyork@gmail.com>
2024-05-13 12:54:25 +0100

    Error message added to the method 'parametrize_mol()'

--- modules/sw_build_systems.py
+++ modules/sw_build_systems.py
@@ -343,7 +343,12 @@ class BuildAmberSystems(BuildSystems):
         with open(file_path, 'w') as file:
             file.writelines(modified_lines)
     
-    def parametrize_mol(self, directories, molecule_name):
+    def parametrize_mol(self, directories=None, molecule_name=None):
+        if directories == None or molecule_name == None:
+            print("Please provide 2 arguments as follows: parametrize_mol(directories, molecule_name)")
+            print("Directories: A python object generated with the PolymerSimulatorDirs(filepath) method imported from sw_directories")
+            print("Molecule name: A string of the molecule name, i.e. 'Ethane'")
+            return(None)
         # Create a new directory for param files for the molecule and copy pdb there
         pdb_filepath = os.path.join(directories.pdb_file_dir, (molecule_name + ".pdb"))
         self.mod_pdb_file(pdb_filepath)
@@ -461,6 +466,7 @@ class BuildAmberSystems(BuildSystems):
         prmtop_filepath =  os.path.join(output_dir, molecule_name + file_subtype + ".prmtop")
         rst_filepath = os.path.join(output_dir, molecule_name + file_subtype + ".rst7")
         three_three_array_pdb_filepath = os.path.join(output_dir, molecule_name + file_subtype + ".pdb")
+        unsolved_three_three_array_pdb_filepath = os.path.join(output_dir, "unsovled_" + molecule_name + file_subtype + ".pdb")
 
         file_content = f"""source leaprc.protein.ff14SB
         source leaprc.gaff
@@ -487,6 +493,8 @@ class BuildAmberSystems(BuildSystems):
          
         system = combine {combine_line}
 
+        savePDB system {unsolved_three_three_array_pdb_filepath}
+        
         solvateBox system TIP3PBOX {box_dist}
 
         saveamberparm system {prmtop_filepath} {rst_filepath}
@@ -589,125 +597,180 @@ class BuildAmberSystems(BuildSystems):
         subprocess.run(leap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     def build_3_3_polymer_array(self, build, directories, molecule_name, number_of_mainchain_units):
-        if build.is_poly_prepped(directories, molecule_name) == True:
+         if build.is_poly_prepped(directories, molecule_name) == True:
             pass
-        if build.is_poly_prepped(directories, molecule_name) == False:
+         if build.is_poly_prepped(directories, molecule_name) == False:
             print("Please prepare prepin files for so polymers can be generated.")
             return()
-        
-        file_subtype = "_3_3_array" + str(number_of_mainchain_units) + "polymer"
-        output_dir = os.path.join(directories.systems_dir, (molecule_name + file_subtype))
-        head_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("head_" + molecule_name + ".prepi"))
-        mainchain_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("mainchain_" + molecule_name + ".prepi"))
-        tail_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("tail_" + molecule_name + ".prepi"))
-        output_dir = os.path.join(directories.systems_dir, (molecule_name.split("_")[0] + file_subtype))
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+             
+         molecule_dir = os.path.join(directories.molecules_dir, molecule_name)
+         cd_command = "cd " + molecule_dir
+         print(cd_command)
 
-        # Also need the pdb_file for pairwise distance (box_size distance), and the pdb_file of the monomer (for x,y distance)
-        pdb_filepath = os.path.join(directories.molecules_dir, molecule_name, (molecule_name + ".pdb"))
-        monomer_name = molecule_name.split("_")[0] + "_monomer"
-        monomer_pdb_filepath = os.path.join(directories.molecules_dir, monomer_name, (monomer_name + ".pdb"))
-    
-        Mol = MolFromPDBFile(pdb_filepath)
-        monomer = MolFromPDBFile(monomer_pdb_filepath)
-        max_dist = self.max_pairwise_distance(monomer)
-        translate_distance = float((int(max_dist)+1))
-    
-        max_dist_box = self.max_pairwise_distance(Mol)
-        box_dist = float((int(max_dist_box)+1)*1.5)
+         molecule_dir = os.path.join(directories.molecules_dir, molecule_name)
 
-        polymer_name = molecule_name.split("_")[0] + "_" + str(number_of_mainchain_units) + "_polymer"
-        molecule_name_1 = polymer_name + "_1"
-        molecule_name_2 = polymer_name + "_2"
-        molecule_name_3 = polymer_name + "_3"
-        molecule_name_4 = polymer_name + "_4"
-        molecule_name_5 = polymer_name + "_5"
-        molecule_name_6 = polymer_name + "_6"
-        molecule_name_7 = polymer_name + "_7"
-        molecule_name_8 = polymer_name + "_8"
-        molecule_name_9 = polymer_name + "_9"
-
-        translate_line_1 = "{0.0 0.0 0.0}"
-        translate_line_2 = "{0.0 0.0 " + str(translate_distance) + "}"
-        translate_line_3 = "{0.0 0.0 " + str(-translate_distance) + "}"
-
-        translate_line_4 = "{0.0 " + str(translate_distance) + " " + str(translate_distance) + "}"
-        translate_line_5 = "{0.0 " + str(translate_distance) + " " + str(-translate_distance) + "}"
-        translate_line_6 = "{0.0 " + str(translate_distance) + " 0.0}"
-
-        translate_line_7 = "{0.0 " + str(-translate_distance) + " " + str(translate_distance) + "}"
-        translate_line_8 = "{0.0 " + str(-translate_distance) + " " + str(-translate_distance) + "}"
-        translate_line_9 = "{0.0 " + str(-translate_distance) + " 0.0}"
-
-        combine_line = "{" + molecule_name_1 + " " + molecule_name_2 + " " + molecule_name_3 + " " + molecule_name_4 + " " + molecule_name_5 + " " + molecule_name_6 + " " + molecule_name_7 + " " + molecule_name_8 + " " + molecule_name_9 + "}"
-
-        base_mol_name = molecule_name.split("_")[0]
-        intleap_path = os.path.join(output_dir, (base_mol_name + file_subtype + ".intleap"))
-        prmtop_filepath =  os.path.join(output_dir, base_mol_name + file_subtype + ".prmtop")
-        rst_filepath = os.path.join(output_dir, base_mol_name + file_subtype + ".rst7")
-        three_three_array_pdb_filepath = os.path.join(output_dir, base_mol_name + file_subtype + ".pdb")
-        unsolved_three_three_array_pdb_filepath = "unsolved_" + three_three_array_pdb_filepath 
-    
-        head_rescode, mainchain_rescode, tail_rescode = directories.retrieve_polymeric_rescodes(directories, "3HB_trimer")
-
-        polymer_code = " ".join([tail_rescode] + [mainchain_rescode] * (number_of_mainchain_units - 1) + [head_rescode])
-        polymer_command = "{" + polymer_code + "}"
-
-        file_content = f"""source leaprc.protein.ff14SB
-        source leaprc.gaff
-        source leaprc.water.fb3
-
-        loadamberprep {head_prepi_filepath}
-        loadamberprep {mainchain_prepi_filepath}
-        loadamberprep {tail_prepi_filepath}
-    
-        {molecule_name_1} = sequence {polymer_command}
-        {molecule_name_2} = sequence {polymer_command}
-        {molecule_name_3} = sequence {polymer_command}
-        {molecule_name_4} = sequence {polymer_command}
-        {molecule_name_5} = sequence {polymer_command}
-        {molecule_name_6} = sequence {polymer_command}
-        {molecule_name_7} = sequence {polymer_command}
-        {molecule_name_8} = sequence {polymer_command}
-        {molecule_name_9} = sequence {polymer_command}
-    
-        translate {molecule_name_1} {translate_line_1}
-        translate {molecule_name_2} {translate_line_2}
-        translate {molecule_name_3} {translate_line_3}
-        translate {molecule_name_4} {translate_line_4}
-        translate {molecule_name_5} {translate_line_5}
-        translate {molecule_name_6} {translate_line_6}
-        translate {molecule_name_7} {translate_line_7}
-        translate {molecule_name_8} {translate_line_8}
-        translate {molecule_name_9} {translate_line_9}
+         try:
+             os.chdir(molecule_dir)
+             print("Current directory:", os.getcwd())
+         except Exception as e:
+             print("Exception:", e)
          
-        system = combine {combine_line}
-
-        savepdb system {unsolved_three_three_array_pdb_filepath}
+         #try:
+          #     result = subprocess.run(cd_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+           #    if result.returncode == 0:
+            #       # Command executed successfully
+             #     print("Output:", result.stdout)
+              # else:
+               #    # Command failed, print error message
+                #   print("Error:", result.stderr)
+          #except Exception as e:
+               # Exception occurred during subprocess execution
+              #print("Exception:", e)
+         
+         file_subtype = "_3_3_array" + str(number_of_mainchain_units) + "polymer"
+         #head_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("head_" + molecule_name + ".prepi"))
+         head_prepi_filepath = "head_" + molecule_name + ".prepi"
     
-        solvateBox system TIP3PBOX {box_dist}
+         #mainchain_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("mainchain_" + molecule_name + ".prepi"))
+         mainchain_prepi_filepath = "mainchain_" + molecule_name + ".prepi"
+    
+         #tail_prepi_filepath = os.path.join(directories.molecules_dir, molecule_name, ("tail_" + molecule_name + ".prepi"))
+         tail_prepi_filepath = "tail_" + molecule_name + ".prepi"
+    
+         output_dir = os.path.join(directories.systems_dir, (molecule_name.split("_")[0] + file_subtype))
+         if not os.path.exists(output_dir):
+             os.makedirs(output_dir)
 
-        saveamberparm system {prmtop_filepath} {rst_filepath}
-        savepdb system {three_three_array_pdb_filepath}
-        quit
-        """
-        with open(intleap_path, 'w') as file:
-            file.write(file_content)
+         # Also need the pdb_file for pairwise distance (box_size distance), and the pdb_file of the monomer (for x,y distance)
+         #pdb_filepath = os.path.join(directories.molecules_dir, molecule_name, (molecule_name + ".pdb"))
+         pdb_filepath = molecule_name + ".pdb"
+    
+         monomer_name = molecule_name.split("_")[0] + "_monomer"
+         monomer_pdb_filepath = os.path.join(directories.molecules_dir, monomer_name, (monomer_name + ".pdb"))
+         current_directory = os.getcwd()
+         print("Current working directory:", current_directory)
+         Mol = MolFromPDBFile(pdb_filepath)
+         monomer = MolFromPDBFile(monomer_pdb_filepath)
+         max_dist = self.max_pairwise_distance(monomer)
+         print("monomer max dist is: ", max_dist)
+         translate_distance = float((int(max_dist)*2))
+    
+         max_dist_box = max_dist*(number_of_mainchain_units+3) # +3 is because the number of mainchain units doesn't account for the head and tsil
+         print("polymer max dist is: ", max_dist_box)
+         box_dist = float((int(max_dist_box)+1))
+
+         polymer_name = molecule_name.split("_")[0] + "_" + str(number_of_mainchain_units) + "_polymer"
+         molecule_name_1 = polymer_name + "_1"
+         molecule_name_2 = polymer_name + "_2"
+         molecule_name_3 = polymer_name + "_3"
+         molecule_name_4 = polymer_name + "_4"
+         molecule_name_5 = polymer_name + "_5"
+         molecule_name_6 = polymer_name + "_6"
+         molecule_name_7 = polymer_name + "_7"
+         molecule_name_8 = polymer_name + "_8"
+         molecule_name_9 = polymer_name + "_9"
+
+         translate_line_1 = "{0.0 0.0 0.0}"
+         translate_line_2 = "{" + str(translate_distance) + " 0.0 0.0}"
+         #translate_line_2 = "{0.0 0.0 " + str(translate_distance) + "}"
+         translate_line_3 = "{" + str(-translate_distance) + " 0.0 0.0}"
+         #translate_line_3 = "{0.0 0.0 " + str(-translate_distance) + "}"
+
+         translate_line_4 = "{" + str(translate_distance) + " " + str(translate_distance) + " 0.0}"
+         #translate_line_4 = "{0.0 " + str(translate_distance) + " " + str(translate_distance) + "}"
+         translate_line_5 = "{" + str(-translate_distance) + " " + str(translate_distance) + " 0.0}"
+         #translate_line_5 = "{0.0 " + str(translate_distance) + " " + str(-translate_distance) + "}"
+         translate_line_6 = "{0.0 " + str(translate_distance) + " 0.0}"
+
+         translate_line_7 = "{" + str(translate_distance) + " " + str(-translate_distance) + " 0.0}"
+         #translate_line_7 = "{0.0 " + str(-translate_distance) + " " + str(translate_distance) + "}"
+         
+         translate_line_8 = "{" + str(-translate_distance) + " " + str(-translate_distance) + " 0.0}"
+         #translate_line_8 = "{0.0 " + str(-translate_distance) + " " + str(-translate_distance) + "}"
+         translate_line_9 = "{0.0 " + str(-translate_distance) + " 0.0}"
+
+         combine_line = "{" + molecule_name_1 + " " + molecule_name_2 + " " + molecule_name_3 + " " + molecule_name_4 + " " + molecule_name_5 + " " + molecule_name_6 + " " + molecule_name_7 + " " + molecule_name_8 + " " + molecule_name_9 + "}"
+
+         base_mol_name = molecule_name.split("_")[0]
+         #intleap_path = os.path.join(output_dir, (base_mol_name + file_subtype + ".intleap"))
+         intleap_path = base_mol_name + file_subtype + ".intleap"
+    
+         prmtop_filepath =  os.path.join(output_dir, base_mol_name + file_subtype + ".prmtop")
+         rst_filepath = os.path.join(output_dir, base_mol_name + file_subtype + ".rst7")
+
+         unsolved_prmtop_filepath =  os.path.join(output_dir, "unsolved_" + base_mol_name + file_subtype + ".prmtop")
+         unsolved_rst_filepath = os.path.join(output_dir, "unsolved_" + base_mol_name + file_subtype + ".rst7")
+        
+         three_three_array_pdb_filepath = os.path.join(output_dir, base_mol_name + file_subtype + ".pdb")
+         unsolved_three_three_array_pdb_filepath = os.path.join(output_dir, "unsolved_" + base_mol_name + file_subtype + ".pdb")
+    
+         head_rescode, mainchain_rescode, tail_rescode = directories.retrieve_polymeric_rescodes(directories, "3HB_trimer")
+
+         print("head_code ", head_rescode)
+         print("tail code ", tail_rescode)
+         print("mainchain_code ", mainchain_rescode)
+         polymer_code = " ".join([head_rescode] + [mainchain_rescode] * (number_of_mainchain_units - 1) + [tail_rescode])
+         polymer_command = "{" + polymer_code + "}"
+
+         file_content = f"""source leaprc.gaff
+         source leaprc.water.fb3
+
+         loadamberprep {head_prepi_filepath}
+         loadamberprep {mainchain_prepi_filepath}
+         loadamberprep {tail_prepi_filepath}
+    
+         {molecule_name_1} = sequence {polymer_command}
+         {molecule_name_2} = sequence {polymer_command}
+         {molecule_name_3} = sequence {polymer_command}
+         {molecule_name_4} = sequence {polymer_command}
+         {molecule_name_5} = sequence {polymer_command}
+         {molecule_name_6} = sequence {polymer_command}
+         {molecule_name_7} = sequence {polymer_command}
+         {molecule_name_8} = sequence {polymer_command}
+         {molecule_name_9} = sequence {polymer_command}
+
+         check molecule_name_1
+         
+         translate {molecule_name_1} {translate_line_1}
+         translate {molecule_name_2} {translate_line_2}
+         translate {molecule_name_3} {translate_line_3}
+         translate {molecule_name_4} {translate_line_4}
+         translate {molecule_name_5} {translate_line_5}
+         translate {molecule_name_6} {translate_line_6}
+         translate {molecule_name_7} {translate_line_7}
+         translate {molecule_name_8} {translate_line_8}
+         translate {molecule_name_9} {translate_line_9}
+         
+         system = combine {combine_line}
+         saveamberparm system {unsolved_prmtop_filepath} {unsolved_rst_filepath}
+         savepdb system {unsolved_three_three_array_pdb_filepath}
+    
+         solvatebox system TIP3PBOX 30 iso
+
+         saveamberparm system {prmtop_filepath} {rst_filepath}
+         savepdb system {three_three_array_pdb_filepath}
+         quit
+         """
+    
+         with open(intleap_path, 'w') as file:
+             file.write(file_content)
             
-        leap_command = "tleap -f " + intleap_path
-        #print("The command that would be run in the shell is: ")
-        #print(leap_command)
-        print(intleap_path)
-        try:
-            result = subprocess.run(leap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if result.returncode == 0:
-                # Command executed successfully
+         leap_command = "tleap -f " + intleap_path
+         #print("The command that would be run in the shell is: ")
+         #print(leap_command)
+         print(intleap_path)
+         try:
+             result = subprocess.run(leap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+             if result.returncode == 0:
+                 # Command executed successfully
                 print("Output:", result.stdout)
-            else:
-                # Command failed, print error message
-                print("Error:", result.stderr)
-        except Exception as e:
-            # Exception occurred during subprocess execution
-            print("Exception:", e)
-        return()
+             else:
+                 # Command failed, print error message
+                 print("Error:", result.stderr)
+         except Exception as e:
+             # Exception occurred during subprocess execution
+             print("Exception:", e)
+
+         cd_command = "cd " + str(directories.main_dir)
+         result = subprocess.run(cd_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+         return()
