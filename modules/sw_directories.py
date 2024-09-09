436f2f30cb9038322dd46de69241d6fa7d85eecc
DanielYyork <daniel.yyork@gmail.com>
2024-09-09 16:45:23 +0100

    Updated. Its been a while since updating this so I do not know what to put here. <insert amazing git commit message here>

--- modules/sw_directories.py
+++ modules/sw_directories.py
@@ -7,15 +7,15 @@ Created on Wed Mar 13 14:18:48 2024
 import os 
 import csv
 '''
-Polymer Simulator Directories Module
+SnippetSim Directories Module
 
-This module defines the PolymerSimulatorDirs class, which is used for initializing
+This module defines the SnippetSimManage class, which is used for initializing
 and organizing directories for polymer simulation setup. It creates specific
 directories such as 'python_scripts', 'pdb_files', 'molecules', 'systems', and manages
 files like 'residue_codes.csv'.
 
 Attributes:
-    main_dir (str): The main directory for polymer simulation setup.
+    main_dir (str): The main directory for snippet simulation setup.
 
 Example:
     >>> from PolymerSimulatorDirs import PolymerSimulatorDirs
@@ -26,11 +26,12 @@ Example:
 Note:
     The main_dir must be provided as a valid path and must already exist.
 '''
-class PolymerSimulatorDirs:
+class SnippetSimManage:
+    packmol_path = "/home/dan/packmol-20.14.4-docs1/packmol-20.14.4-docs1/packmol"
     # Class for intialsing and building files and folders for polymer simulation set up and the like
     def __init__(self, main_dir): 
         """
-        Initialize PolymerSimulatorDirs object.
+        Initialize SnippetSimManage object.
 
         Args:
             main_dir (str): The main directory for polymer simulation setup.
@@ -76,20 +77,24 @@ class PolymerSimulatorDirs:
         self.systems_dir = os.path.join(self.pdb_file_dir, 'systems')   
         if not os.path.exists(self.systems_dir):
             os.makedirs(self.systems_dir)
+        
 
-    def mol2_files_avail(self, directories):
+    def mol2_files_avail(self):
+        mol2_avail = []
         # Walk through the directory tree recursively
-        for root, dirs, files in os.walk(directories.pdb_file_dir):
+        for root, dirs, files in os.walk(self.pdb_file_dir):
             dirs[:] = [d for d in dirs if d != 'depreceated']
             # Check each file in the current directory
             for file in files:
                 # Check if the file has a .pdb extension
                 if file.endswith(".mol2"):
                     # Construct the full path to the .pdb file
-                    mol2_file_path = os.path.join(root, file)
+                    mol2_filepath = os.path.join(root, file)
+                    mol2_avail.append(mol2_filepath)
                     # Extract molecule name
-                    mol2_file = mol2_file_path.split("/")[-1]
-                    print(mol2_file)
+                    mol2_file = mol2_filepath.split("/")[-1]
+                    #print(mol2_file)
+        return(mol2_avail)
 
     def load_mol2_filepath(self, molecule_name=None):
         if molecule_name == None:
@@ -104,12 +109,13 @@ class PolymerSimulatorDirs:
             for file in files:
                 # Check if the file has a .pdb extension
                 if file.endswith(".mol2"):
-                    if molecule_name in file:
+                    if (molecule_name + ".mol2") == file:
+                        #print(file)
                         mol2_file_path = os.path.join(root, file)
                         return mol2_file_path 
 
-    def load_pckml_filepath(self, molecule_name=None):
-        if molecule_name == None:
+    def load_pckml_filepath(self, system_name=None):
+        if system_name == None:
             print("Please provide the name of the system you are retrieving files as follows: 'pckml_file = directories.load_pckml_filepath('pb_ph_41')")
             print("NOTE: Packmol input files must be written manually.")
             return(None)
@@ -120,11 +126,12 @@ class PolymerSimulatorDirs:
             for file in files:
                 # Check if the file has a .pdb extension
                 if file.endswith(".pckml"):
-                    if molecule_name in file:
+                    if (system_name + ".pckml") == file:
                         pckml_file_path = os.path.join(root, file)
                         return pckml_file_path 
                         
     def pckml_files_avail(self):
+        pckml_avail = []
         # Walk through the directory tree recursively
         for root, dirs, files in os.walk(self.pdb_file_dir):
             dirs[:] = [d for d in dirs if d != 'depreceated']
@@ -133,12 +140,15 @@ class PolymerSimulatorDirs:
                 # Check if the file has a .pdb extension
                 if file.endswith(".pckml"):
                     # Construct the full path to the .pdb file
-                    pckml_file_path = os.path.join(root, file)
+                    pckml_filepath = os.path.join(root, file)
+                    pckml_avail.append(pckml_filepath)
                     # Extract molecule name
-                    pckml_file = pckml_file_path.split("/")[-1]
-                    print(pckml_file)
-                    
+                    pckml_file = pckml_filepath.split("/")[-1]
+                    #print(pckml_file)
+        return(pckml_avail)
+
     def pdb_files_avail(self):
+        pdb_avail = []
         # Walk through the directory tree recursively
         for root, dirs, files in os.walk(self.pdb_file_dir):
             dirs[:] = [d for d in dirs if d != 'depreceated']
@@ -147,25 +157,13 @@ class PolymerSimulatorDirs:
                 # Check if the file has a .pdb extension
                 if file.endswith(".pdb"):
                     # Construct the full path to the .pdb file
-                    pdb_file_path = os.path.join(root, file)
+                    pdb_filepath = os.path.join(root, file)
+                    pdb_avail.append(pdb_filepath)
                     # Extract molecule name
-                    pdb_file = pdb_file_path.split("/")[-1]
-                    print(pdb_file)
+                    pdb_file = pdb_filepath.split("/")[-1]
+                    #print(pdb_file)
+        return(pdb_avail)
 
-    def ac_files_avail(self):
-        # Walk through the directory tree recursively
-        for root, dirs, files in os.walk(self.pdb_file_dir):
-            dirs[:] = [d for d in dirs if d != 'depreceated']
-            # Check each file in the current directory
-            for file in files:
-                # Check if the file has a .pdb extension
-                if file.endswith(".ac"):
-                    # Construct the full path to the .pdb file
-                    pdb_file_path = os.path.join(root, file)
-                    # Extract molecule name
-                    pdb_file = pdb_file_path.split("/")[-1]
-                    print(pdb_file)
-    
     def load_pdb_filepath(self, molecule_name=None):
         if molecule_name == None:
             print("Please provide the name of the system you are retrieving files as follows: 'pdb_file = directories.load_pdb_filepath('ethane')")
@@ -182,27 +180,29 @@ class PolymerSimulatorDirs:
                 if file == (molecule_name + ".pdb"):
                     #if molecule_name in file:
                     pdb_file_path = os.path.join(root, file)
-                    return pdb_file_path             
-    
-    def parametrized_mols_avail(self):
-        a = False
-        for root, dirs, files in os.walk(self.molecules_dir):
+                    return pdb_file_path 
+                    
+    def ac_files_avail(self):
+        ac_avail = []
+        # Walk through the directory tree recursively
+        for root, dirs, files in os.walk(self.pdb_file_dir):
             dirs[:] = [d for d in dirs if d != 'depreceated']
             # Check each file in the current directory
             for file in files:
-                # Check if the file has a .pdb extension
-                if file.endswith(".mol2"):
-                    a = True
-                    # Construct the full path to the .pdb file
-                    pdb_file_path = os.path.join(root, file)
+                # Check if the file has a .ac extension
+                if file.endswith(".ac"):
+                    # Construct the full path to the .ac file
+                    ac_filepath = os.path.join(root, file)
+                    ac_avail.append(ac_filepath)
                     # Extract molecule name
-                    pdb_file = pdb_file_path.split("/")[-1]
-                    print(pdb_file)
-        if a == False:
-            print("No parametrized molecules.")
+                    ac_file = ac_filepath.split("/")[-1]
+                    #print(ac_file)
+        return(ac_avail)
+                
             
     def amber_systems_avail(self):
         a = False
+        amber_system_avail =[]
         for root, dirs, files in os.walk(self.systems_dir):
             dirs[:] = [d for d in dirs if d != 'depreceated']
             # Check each file in the current directory
@@ -211,24 +211,30 @@ class PolymerSimulatorDirs:
                 if file.endswith(".prmtop"):
                     a = True
                     # Construct the full path to the .pdb file
-                    pdb_file_path = os.path.join(root, file)
+                    prmtop_filepath = os.path.join(root, file)
                     # Extract molecule name
-                    pdb_file = pdb_file_path.split("/")[-1]
-                    print(pdb_file)
+                    prmtop_file = prmtop_filepath.split("/")[-1]
+                    amber_system_avail.append(prmtop_file)
+                    #print(pdb_file)
                 if file.endswith(".rst7"):
                     a = True
                     # Construct the full path to the .pdb file
-                    pdb_file_path = os.path.join(root, file)
+                    rst7_filepath = os.path.join(root, file)
+                    amber_system_avail.append(rst7_filepath)
                     # Extract molecule name
-                    pdb_file = pdb_file_path.split("/")[-1]
-                    print(pdb_file)
+                    rst7_file = rst7_filepath.split("/")[-1]
+                    #print(pdb_file)
         if a == True:
             print("")
-            print("Remember you need both .prmtop and .rst7 files to run a simulation")               
+            print("Remember you need both .prmtop and .rst7 files to run a simulation")    
+            return(amber_system_avail)
         if a == False:
             print("No parametrized molecules.")
+            return(None)
 
-    def retrieve_top_crds(self, system_name=None):
+    def load_amber_filepaths(self, system_name=None):
+        prmtop_file_path = None
+        coord_file_path = None
         if system_name == None:
             print("Please provide the name of the system you are retrieving files as follows: 'topology_file, coordinate_file = directories.retrieve_top_crds('ethane')")
             print("Change ethane for the name of the desired system")
@@ -246,10 +252,15 @@ class PolymerSimulatorDirs:
                 #if file.endswith(".rst7") and system_name in file:
                 if file == (system_name + ".rst7"):
                     # Construct the full path to the .pdb file
-                    coord_file_path = os.path.join(root, file)   
-        return(prmtop_file_path, coord_file_path)
+                    coord_file_path = os.path.join(root, file)
+        if (prmtop_file_path is not None) and (coord_file_path is not None):
+            return(prmtop_file_path, coord_file_path)
+        else:
+            print("Files not found. Check name of molecule/system and if files have been generated.")
+            return(None)
 
     def retrieve_files_for_MDanalysis(self, system_name=None):
+        prmtop_file_path = None
         if system_name == None:
             print("Please provide the name of the system you are retrieving files for.")
             return(None)
@@ -321,6 +332,105 @@ class PolymerSimulatorDirs:
                 if len(parts.split(",")) >= 3 and parts.split(",")[0] == ("tail_" + molecule_name):
                     tail_code = parts.split(",")[2]    
         return(head_code, mainchain_code, tail_code)
+
+
+    def retrieve_rescode(self, molecule_name):
+        with open(self.residue_code_csv, 'r') as file:
+            rescode = None
+            for line in file:
+                parts = (line.strip().split('\t'))[0]
+                if len(parts.split(",")) >= 3 and parts.split(",")[0] == (molecule_name):
+                    rescode = parts.split(",")[2]
+        return(rescode)
+
+
+        
     # Need to add stuff for md results and the like
     # Need to add a way to import the bash submission scripts - will get to this at some point
-    
+
+        if not os.path.exists(main_dir):
+            raise FileNotFoundError(f"The specified main directory '{main_dir}' does not exist.")       
+        self.main_dir = main_dir 
+        
+        # Get path to python script directory and check if exists
+        self.python_script_dir = os.path.join(main_dir, 'python_scripts')        
+        if not os.path.exists(self.python_script_dir):
+            os.makedirs(self.python_script_dir)
+         
+        # Get path to pdb file directory and check if exists
+        self.pdb_file_dir = os.path.join(main_dir, 'pdb_files')      
+        if not os.path.exists(self.pdb_file_dir):
+            os.makedirs(self.pdb_file_dir)     
+         
+        # Get path to csv to pdb file directory and check if exists
+        self.csv_to_pdb_dir = os.path.join(main_dir, 'csvs_to_pdb')
+        if not os.path.exists(self.csv_to_pdb_dir):
+            os.makedirs(self.csv_to_pdb_dir)
+         
+        # Get path to residue code csv and check if exists
+        self.residue_code_csv = os.path.join(self.pdb_file_dir, 'residue_codes.csv')       
+        if not os.path.exists(self.residue_code_csv):
+            with open(self.residue_code_csv, 'w') as file:
+                pass # Don't want to do anything with it, just generate it
+        else:
+            pass # Pass as the file already exists
+        
+        # Get path to molecules directory and check if exists - this will contain the parameters and pdb for individual molecules
+        self.molecules_dir = os.path.join(self.pdb_file_dir, 'molecules')     
+        if not os.path.exists(self.molecules_dir):
+            os.makedirs(self.molecules_dir)
+
+        
+        # Get path to systems directory and check if exists - this will contain the parameters and pdb for systems for md simulations
+        self.systems_dir = os.path.join(self.pdb_file_dir, 'systems')   
+        if not os.path.exists(self.systems_dir):
+            os.makedirs(self.systems_dir)
+            
+class BioOilDirs(SnippetSimManage):
+
+    def __init__(self, main_dir, *args, **kwargs):
+        """
+        Initialize PolymerSimulatorDirs object.
+
+        Args:
+            main_dir (str): The main directory for polymer simulation setup.
+                            Example: '/path/to/main/dir/'
+
+        Raises:
+            FileNotFoundError: If main_dir does not exist.
+        """
+        # Call the parent class's __init__ method with all arguments
+        super().__init__(main_dir, *args, **kwargs)
+        
+        if not os.path.exists(main_dir):
+            raise FileNotFoundError
+
+        self.bio_oil_dir = os.path.join(main_dir, 'bio_oil')
+        if not os.path.exists(self.bio_oil_dir):
+            os.makedirs(self.bio_oil_dir)
+
+        self.bio_oil_systems_dir = os.path.join(self.bio_oil_dir, 'systems')
+        if not os.path.exists(self.bio_oil_systems_dir):
+            os.makedirs(self.bio_oil_systems_dir)
+
+        self.bio_oil_GC_data = os.path.join(self.bio_oil_dir, 'GC_data')
+        if not os.path.exists(self.bio_oil_GC_data):
+            os.makedirs(self.bio_oil_GC_data)
+
+        self.bio_oil_models_dir = os.path.join(self.bio_oil_dir, 'models')
+        if not os.path.exists(self.bio_oil_models_dir):
+            os.makedirs(self.bio_oil_models_dir)
+
+    def GC_data_avail(self):
+        # Walk through the directory tree recursively
+        for root, dirs, files in os.walk(self.bio_oil_GC_data):
+            dirs[:] = [d for d in dirs if d != 'depreceated']
+            # Check each file in the current directory
+            for file in files:
+                # Check if the file has a .pdb extension
+                if file.endswith(".csv"):
+                    # Construct the full path to the .csv file
+                    csv_file_path = os.path.join(root, file)
+                    # Extract filename
+                    csv_file = csv_file_path.split("/")[-1]
+                    print(csv_file)
