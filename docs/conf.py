38978b09780f8c9d937e50c334dad25fc6f9bb98
DanielYyork <daniel.yyork@gmail.com>
2025-01-06 14:10:34 +0000

    Updated configuration file

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,24 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))  # Adjusting to root project directory
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project information
 project = 'Polymer Simulator'
 copyright = '2025, Daniel J. York'
 author = 'Daniel J. York'
 release = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Sphinx extensions
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',  # Optional for Google/NumPy docstring support
+]
 
-extensions = []
-
+# Paths and templates
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output
 html_theme = 'alabaster'
 html_static_path = ['_static']
