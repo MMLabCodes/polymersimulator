3650c51ebedfe375fa09ac975a478fcbbfe2a682
DanielYyork <daniel.yyork@gmail.com>
2025-01-09 10:44:25 +0000

    configuration path to sw_openmm.py modified

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))  # Adjusting to root project directory
-sys.path.insert(0, os.path.abspath('../modules/')) 
+sys.path.insert(0, os.path.abspath('../modules/sw_openmm.py')) 
 # Project information
 project = 'Polymer Simulator'
 copyright = '2025, Daniel J. York'
