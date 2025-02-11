65d1858ca73ca15563c1ab3b9dfc87ecd04d752b
DanielYyork <daniel.yyork@gmail.com>
2025-02-11 15:20:50 +0000

    Function added to generate amber parameters for a polymer

--- modules/sw_build_systems.py
+++ modules/sw_build_systems.py
@@ -2079,6 +2079,63 @@ class BuildAmberSystems(BuildSystems):
             # Exception occurred during subprocess execution
             print("Exception:", e)
         return(output_name)  
+
+    def gen_amber_params_sing_poly(self, base_molecule_name, polymer_name):
+
+        head_prepi_file = os.path.join(self.manager.molecules_dir, base_molecule_name, ("head_" + base_molecule_name + ".prepi"))
+        mainchain_prepi_file = os.path.join(self.manager.molecules_dir, base_molecule_name, ("mainchain_" + base_molecule_name + ".prepi"))
+        tail_prepi_file = os.path.join(self.manager.molecules_dir, base_molecule_name, ("tail_" + base_molecule_name + ".prepi"))
+        base_molecule_frcmod_file = os.path.join(self.manager.molecules_dir, base_molecule_name, (base_molecule_name + ".frcmod"))
+        polymer_pdb_file = os.path.join(self.manager.systems_dir, polymer_name, (polymer_name + ".pdb"))
+
+        file_subtype = "_sing_poly"
+        output_name = polymer_name + file_subtype
+
+        output_dir = os.path.join(self.manager.systems_dir, output_name)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+
+        intleap_path = os.path.join(output_dir, output_name + ".intleap")
+        prmtop_filepath = os.path.join(output_dir, output_name + ".prmtop")
+        rst_filepath = os.path.join(output_dir, output_name + ".rst7")
+        pdb_filepath = os.path.join(output_dir, output_name + ".pdb")
+
+        file_content = f"""source leaprc.gaff
+             source leaprc.water.fb3
+             source leaprc.protein.ff14SB
+
+             loadamberprep {head_prepi_file}
+             loadamberprep {mainchain_prepi_file}
+             loadamberprep {tail_prepi_file}
+             loadamberparams {base_molecule_frcmod_file}
+
+             list
+
+             system = loadpdb {polymer_pdb_file}
+             setBox system vdw 0.0
+             saveamberparm system {prmtop_filepath} {rst_filepath}
+             savepdb system {pdb_filepath}
+             quit
+             """
+        with open(intleap_path, 'w') as file:
+             file.write(file_content)
+
+        leap_command = "tleap -f " + intleap_path
+
+        try:
+            result = subprocess.run(leap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if result.returncode == 0:
+               # Command executed successfully
+               print("Output:", result.stdout)
+            else:
+                # Command failed, print error message
+                print("Error:", result.stderr)
+        except Exception as e:
+            # Exception occurred during subprocess execution
+            print("Exception:", e)
+        return(output_name)  
+        
 #class BuildBioOilSystems(BuildSystems):
 #    def __init__:
  #       pass
